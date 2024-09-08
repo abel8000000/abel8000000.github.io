f4dd39077ec55cacb0b16bb7d110c99f63643ef2
Abel Garcia <abel22561@gmail.com>
2024-09-08 13:19:56 -0500

    Mobile resizing, links, styling.

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/background.css';
 import purplePlanetEarth from '../assets/images/purple-planet-earth.gif';
 import myWorldMarquee from '../assets/images/my-world-marquee.png';
 import backgroundImage from '../assets/images/background.png';
 
 const Background = () => {
+    useEffect(() => {
+        const setVh = () => {
+            const vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        };
+
+        setVh();
+        window.addEventListener('resize', setVh);
+
+        return () => {
+            window.removeEventListener('resize', setVh);
+        };
+    }, []);
+
     const backgroundStyle = {
         backgroundImage: `url(${backgroundImage})`,
         backgroundSize: 'cover',
         position: 'relative',
         width: '100vw',
-        height: '100vh',
+        height: 'calc(var(--vh, 1vh) * 100)',
         overflow: 'hidden',
         margin: '0',
     };
--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -4,32 +4,38 @@ import BlogIcon from '../assets/images/blog-icon.png';
 import IeIcon from '../assets/images/ie-icon.png';
 import githubIcon from '../assets/images/github-icon.png';
 import instagramIcon from '../assets/images/instagram-icon.png';
+import '../styles/desktop.css';
 
 const Desktop = () => {
     const [showBlogWindow, setShowBlogWindow] = useState(false);
     const [showThoughtsWindow, setShowThoughtsWindow] = useState(false);
 
     const handleCloseBlogWindow = () => setShowBlogWindow(false);
+    const handleOpenBlogWindow = () => setShowBlogWindow(true);
     const handleCloseThoughtsWindow = () => setShowThoughtsWindow(false);
+    const handleOpenThoughtsWindow = () => setShowThoughtsWindow(true);
+
+    const isMobile = window.innerWidth < 768;
 
     return (
         <>
-            <Button className='desktopIcon' id='blog-icon' onClick={() => setShowBlogWindow(true)}>
+            <Button className='desktopIcon' id='blog-icon' onClick={handleOpenBlogWindow} onTouchEnd={handleOpenBlogWindow}>
                 <img src={BlogIcon} alt='Blog' />
                 <span>Blog</span>
             </Button>
-            <Button className='desktopIcon' id='thoughts-icon' onClick={() => setShowThoughtsWindow(true)}>
+            <Button className='desktopIcon' id='thoughts-icon' onClick={handleOpenThoughtsWindow} onTouchEnd={handleOpenThoughtsWindow}>
                 <img src={IeIcon} alt='Internet Explorer' />
                 <span>Thoughts</span>
             </Button>
 
             {showBlogWindow && <Modal
-                width="300px"
-                height="200px"
+                width={isMobile ? '95vw' : '90vw'}
+                height={isMobile ? '85vh' : '80vh'}
+
                 title="Blog"
                 zIndex={1}
                 icon=<img src={BlogIcon} alt='Blog' width={16} height={16} />
-                defaultPosition={{ x: 100, y: 100 }}
+                defaultPosition={isMobile ? { x: 10, y: 10 } : { x: 40, y: 5 }}
                 onClose={handleCloseBlogWindow}
             >
                 <Frame bg="white" boxShadow="$in" h="100%" w="100%" padding="0px 5px">
@@ -40,11 +46,11 @@ const Desktop = () => {
             </Modal>}
 
             {showThoughtsWindow && <Modal
-                width="300px"
-                height="200px"
+                width={isMobile ? '95vw' : '90vw'}
+                height={isMobile ? '85vh' : '80vh'}
                 title="Thoughts"
                 zIndex={1}
-                defaultPosition={{ x: 100, y: 100 }}
+                defaultPosition={isMobile ? { x: 10, y: 20 } : { x: 45, y: 10 }}
                 onClose={handleCloseThoughtsWindow}
             >
                 <Frame bg="white" boxShadow="$in" h="100%" w="100%" padding="0px 5px">
@@ -58,12 +64,16 @@ const Desktop = () => {
                 list={
                     <List>
                         <TitleBar title='Made with <3 by Abel' />
-                        <List.Item className='taskbarIcon' id='github' icon={<img src={githubIcon} alt="GitHub" />}>
-                            abel8000000
-                        </List.Item>
-                        <List.Item className='taskbarIcon' id='instagram' icon={<img src={instagramIcon} alt="Instagram" />}>
-                            abel8000000
-                        </List.Item>
+                        <a href='https://github.com/abel8000000' target='_blank' rel='noopener noreferrer'>
+                            <List.Item className='taskbarIcon' id='github' icon={<img src={githubIcon} alt="GitHub" />}>
+                                abel8000000
+                            </List.Item>
+                        </a>
+                        <a href='https://www.instagram.com/abel8000000/' target='_blank' rel='noopener noreferrer'>
+                            <List.Item className='taskbarIcon' id='instagram' icon={<img src={instagramIcon} alt="Instagram" />}>
+                                abel8000000
+                            </List.Item>
+                        </a>
                     </List>
                 }
             />
