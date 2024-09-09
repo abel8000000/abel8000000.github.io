286c38a4326b79f9212b11d69b8b7af18413dcaa
Abel Garcia <abel22561@gmail.com>
2024-09-08 19:06:46 -0500

    Added blog window layout

---
+++ src/components/BlogWindow.jsx
@@ -0,0 +1,175 @@
+import React, { useState } from 'react';
+import { Button, Frame } from '@react95/core';
+import '../styles/blog-window.css';
+
+import BlogBackIconGray from '../assets/blog-window-icons/blog-back-icon-gray.png';
+import BlogBackIconColor from '../assets/blog-window-icons/blog-back-icon-color.png';
+import BlogForwardIconGray from '../assets/blog-window-icons/blog-forward-icon-gray.png';
+import BlogForwardIconColor from '../assets/blog-window-icons/blog-forward-icon-color.png';
+import BlogStopIconGray from '../assets/blog-window-icons/blog-stop-icon-gray.png';
+import BlogStopIconColor from '../assets/blog-window-icons/blog-stop-icon-color.png';
+import BlogRefreshIconGray from '../assets/blog-window-icons/blog-refresh-icon-gray.png';
+import BlogRefreshIconColor from '../assets/blog-window-icons/blog-refresh-icon-color.png';
+import BlogHomeIconGray from '../assets/blog-window-icons/blog-home-icon-gray.png';
+import BlogHomeIconColor from '../assets/blog-window-icons/blog-home-icon-color.png';
+import BlogSearchIconGray from '../assets/blog-window-icons/blog-search-icon-gray.png';
+import BlogSearchIconColor from '../assets/blog-window-icons/blog-search-icon-color.png';
+import BlogFavoritesIconGray from '../assets/blog-window-icons/blog-favorites-icon-gray.png';
+import BlogFavoritesIconColor from '../assets/blog-window-icons/blog-favorites-icon-color.png';
+import BlogHistoryIconGray from '../assets/blog-window-icons/blog-history-icon-gray.png';
+import BlogHistoryIconColor from '../assets/blog-window-icons/blog-history-icon-color.png';
+import BlogMailIconGray from '../assets/blog-window-icons/blog-mail-icon-gray.png';
+import BlogMailIconColor from '../assets/blog-window-icons/blog-mail-icon-color.png';
+import BlogPrintIconGray from '../assets/blog-window-icons/blog-print-icon-gray.png';
+import BlogPrintIconColor from '../assets/blog-window-icons/blog-print-icon-color.png';
+
+const BlogWindow = () => {
+    const [hoveredButton, setHoveredButton] = useState(null);
+
+    const handleMouseEnter = (buttonName) => {
+        setHoveredButton(buttonName);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredButton(null);
+    };
+
+    return (
+        <>
+            <Frame id="toolbar">
+                <div className="toolbar-drag-handle"></div>
+                <Button
+                    disabled
+                    className='blogIcon'
+                    onMouseEnter={() => handleMouseEnter('back')}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <img
+                        className='blogIconImage'
+                        src={hoveredButton === 'back' ? BlogBackIconColor : BlogBackIconGray}
+                        alt="Back"
+                    />
+                    <span>Back</span>
+                </Button>
+                <Button
+                    disabled
+                    className='blogIcon'
+                    onMouseEnter={() => handleMouseEnter('forward')}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <img
+                        className='blogIconImage'
+                        src={hoveredButton === 'forward' ? BlogForwardIconColor : BlogForwardIconGray}
+                        alt="Forward"
+                    />
+                    <span>Forward</span>
+                </Button>
+                <Button
+                    className='blogIcon'
+                    onMouseEnter={() => handleMouseEnter('stop')}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <img
+                        className='blogIconImage'
+                        src={hoveredButton === 'stop' ? BlogStopIconColor : BlogStopIconGray}
+                        alt="Stop"
+                    />
+                    <span>Stop</span>
+                </Button>
+                <Button
+                    className='blogIcon'
+                    onMouseEnter={() => handleMouseEnter('refresh')}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <img
+                        className='blogIconImage'
+                        src={hoveredButton === 'refresh' ? BlogRefreshIconColor : BlogRefreshIconGray}
+                        alt="Refresh"
+                    />
+                    <span>Refresh</span>
+                </Button>
+                <Button
+                    className='blogIcon'
+                    onMouseEnter={() => handleMouseEnter('home')}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <img
+                        className='blogIconImage'
+                        src={hoveredButton === 'home' ? BlogHomeIconColor : BlogHomeIconGray}
+                        alt="Home"
+                    />
+                    <span>Home</span>
+                </Button>
+                <hr aria-orientation="vertical" />
+                <Button
+                    className='blogIcon'
+                    onMouseEnter={() => handleMouseEnter('search')}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <img
+                        className='blogIconImage'
+                        src={hoveredButton === 'search' ? BlogSearchIconColor : BlogSearchIconGray}
+                        alt="Search"
+                    />
+                    <span>Search</span>
+                </Button>
+                <Button
+                    className='blogIcon'
+                    onMouseEnter={() => handleMouseEnter('favorites')}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <img
+                        className='blogIconImage'
+                        src={hoveredButton === 'favorites' ? BlogFavoritesIconColor : BlogFavoritesIconGray}
+                        alt="Favorites"
+                    />
+                    <span>Favorites</span>
+                </Button>
+                <Button
+                    className='blogIcon'
+                    onMouseEnter={() => handleMouseEnter('history')}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <img
+                        className='blogIconImage'
+                        src={hoveredButton === 'history' ? BlogHistoryIconColor : BlogHistoryIconGray}
+                        alt="History"
+                    />
+                    <span>History</span>
+                </Button>
+                <hr aria-orientation="vertical" />
+                <Button
+                    className='blogIcon'
+                    onMouseEnter={() => handleMouseEnter('mail')}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <img
+                        className='blogIconImage'
+                        src={hoveredButton === 'mail' ? BlogMailIconColor : BlogMailIconGray}
+                        alt="Mail"
+                    />
+                    <span>Mail</span>
+                </Button>
+                <Button
+                    className='blogIcon'
+                    onMouseEnter={() => handleMouseEnter('print')}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <img
+                        className='blogIconImage'
+                        src={hoveredButton === 'print' ? BlogPrintIconColor : BlogPrintIconGray}
+                        alt="Print"
+                    />
+                    <span>Print</span>
+                </Button>
+            </Frame>
+            <Frame bg="white" boxShadow="$in" h="90%" w="100%" padding="0px 5px">
+                <p>
+                    Blog
+                </p>
+            </Frame>
+        </>
+    );
+};
+
+export default BlogWindow;
--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { Button, Modal, List, Frame, TaskBar, TitleBar } from '@react95/core';
+import { Button, Modal, List, Frame, TaskBar, TitleBar, Checkbox, RadioButton } from '@react95/core';
 import BlogIcon from '../assets/images/blog-icon.png';
 import IeIcon from '../assets/images/ie-icon.png';
 import githubIcon from '../assets/images/github-icon.png';
 import instagramIcon from '../assets/images/instagram-icon.png';
 import '../styles/desktop.css';
+import BlogWindow from './BlogWindow.jsx';
 
 const Desktop = () => {
     const [showBlogWindow, setShowBlogWindow] = useState(false);
@@ -15,39 +16,210 @@ const Desktop = () => {
     const handleCloseThoughtsWindow = () => setShowThoughtsWindow(false);
     const handleOpenThoughtsWindow = () => setShowThoughtsWindow(true);
 
+    // For the radio buttons in the Blog window
+    const [selectedOption, /* setSelectedOption */] = React.useState('medium');
+    // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setSelectedOption(e.target.value);
+
     const isMobile = window.innerWidth < 768;
 
     return (
         <>
-            <Button className='desktopIcon' id='blog-icon' onClick={handleOpenBlogWindow} onTouchEnd={handleOpenBlogWindow}>
-                <img src={BlogIcon} alt='Blog' />
-                <span>Blog</span>
-            </Button>
-            <Button className='desktopIcon' id='thoughts-icon' onClick={handleOpenThoughtsWindow} onTouchEnd={handleOpenThoughtsWindow}>
-                <img src={IeIcon} alt='Internet Explorer' />
-                <span>Thoughts</span>
-            </Button>
+            <div className='desktopIcon' id='blog-icon'>
+                <Button onClick={handleOpenBlogWindow}>
+                    <img src={BlogIcon} alt='Blog' />
+                    <span>Blog</span>
+                </Button>
+            </div>
+            <div className='desktopIcon' id='thoughts-icon'>
+                <Button onClick={handleOpenThoughtsWindow}>
+                    <img src={IeIcon} alt='Internet Explorer' />
+                    <span>Thoughts</span>
+                </Button>
+            </div>
 
             {showBlogWindow && <Modal
                 width={isMobile ? '95vw' : '90vw'}
+                maxWidth="1280px"
                 height={isMobile ? '85vh' : '80vh'}
-
+                maxHeight="720px"
                 title="Blog"
                 zIndex={1}
-                icon=<img src={BlogIcon} alt='Blog' width={16} height={16} />
-                defaultPosition={isMobile ? { x: 10, y: 10 } : { x: 40, y: 5 }}
+                icon=<img className='windowIcon' src={BlogIcon} alt='Blog' width={16} height={16} />
+                defaultPosition={isMobile ? { x: 10, y: 10 } : { x: 60, y: 20 }}
                 onClose={handleCloseBlogWindow}
+                menu={[{
+                    name: 'File',
+                    list: <List width="200px">
+                        <List.Item>
+                            <List width="150px">
+                                <List.Item>Window</List.Item>
+                                <List.Divider />
+                                <List.Item>Message</List.Item>
+                                <List.Item>Post</List.Item>
+                                <List.Item>Contact</List.Item>
+                                <List.Item>Internet Call</List.Item>
+                            </List>
+                            New
+                        </List.Item>
+                        <List.Item>Open...</List.Item>
+                        <List.Item>Edit</List.Item>
+                        <List.Item>Save</List.Item>
+                        <List.Item>Save As...</List.Item>
+                        <List.Divider />
+                        <List.Item>Page Setup...</List.Item>
+                        <List.Item>Print...</List.Item>
+                        <List.Divider />
+                        <List.Item>
+                            <List width="150px">
+                                <List.Item>Page by E-mail...</List.Item>
+                                <List.Item>Link by E-mail...</List.Item>
+                                <List.Item>Shortcut to Desktop</List.Item>
+                            </List>
+                            Send
+                        </List.Item>
+                        <List.Item>Import and Export...</List.Item>
+                        <List.Divider />
+                        <List.Item>Properties</List.Item>
+                        <List.Item>Work Offline</List.Item>
+                        <List.Item onClick={handleCloseBlogWindow}>Close</List.Item>
+                    </List>
+                }, {
+                    name: 'Edit',
+                    list: <List width="200px">
+                        <List.Item>Cut</List.Item>
+                        <List.Item>Copy</List.Item>
+                        <List.Item>Paste</List.Item>
+                        <List.Divider />
+                        <List.Item>Select All</List.Item>
+                        <List.Divider />
+                        <List.Item>Find (on This Page)...</List.Item>
+                    </List>
+                }, {
+                    name: 'View',
+                    list: <List width="200px">
+                        <List.Item>
+                            <List width="150px">
+                                <Checkbox readOnly checked>Standard Buttons</Checkbox>
+                                <br />
+                                <Checkbox readOnly checked>Address Bar</Checkbox>
+                                <br />
+                                <Checkbox readOnly checked>Links</Checkbox>
+                                <br />
+                                <Checkbox readOnly checked={false}>Radio</Checkbox>
+                                <br />
+                                <List.Divider />
+                                <List.Item>Customize...</List.Item>
+                            </List>
+                            Toolbars
+                        </List.Item>
+                        <Checkbox readOnly checked>Status Bar</Checkbox>
+                        <List.Item>
+                            <List width="150px">
+                                <List.Item>Search</List.Item>
+                                <List.Item>Favorites</List.Item>
+                                <List.Item>History</List.Item>
+                                <List.Item>Folders</List.Item>
+                                <List.Divider />
+                                <List.Item>Tip of the Day</List.Item>
+                            </List>
+                            Explorer Bar
+                        </List.Item>
+                        <List.Divider />
+                        <List.Item>
+                            <List width="150px">
+                                <List.Item>Back</List.Item>
+                                <List.Item>Forward</List.Item>
+                                <List.Divider />
+                                <List.Item>Home Page</List.Item>
+                                <List.Divider />
+                                <Checkbox readOnly checked={false}>Cannot find server</Checkbox>
+                            </List>
+                            Go To
+                        </List.Item>
+                        <List.Item>Stop</List.Item>
+                        <List.Item>Refresh</List.Item>
+                        <List.Divider />
+                        <List.Item>
+                            <List width="150px">
+                                <RadioButton name="text-size" value="largest" checked={selectedOption === 'largest'} onChange={null /* TODO */}>
+                                    Largest
+                                </RadioButton>
+                                <RadioButton name="text-size" value="larger" checked={selectedOption === 'larger'} onChange={null /* TODO */}>
+                                    Larger
+                                </RadioButton>
+                                <RadioButton name="text-size" value="medium" checked={selectedOption === 'medium'} onChange={null /* TODO */}>
+                                    Medium
+                                </RadioButton>
+                                <RadioButton name="text-size" value="smaller" checked={selectedOption === 'smaller'} onChange={null /* TODO */}>
+                                    Smaller
+                                </RadioButton>
+                                <RadioButton name="text-size" value="smallest" checked={selectedOption === 'smallest'} onChange={null /* TODO */}>
+                                    Smallest
+                                </RadioButton>
+                            </List>
+                            Text Size
+                        </List.Item>
+                        <List.Item>
+                            <List width="150px">
+                                <Checkbox readOnly checked>Auto-Select</Checkbox>
+                            </List>
+                            Encoding
+                        </List.Item>
+                        <List.Divider />
+                        <List.Item>Source</List.Item>
+                        <List.Item>Full Screen</List.Item>
+                    </List>
+                }, {
+                    name: 'Favorites',
+                    list: <List width="200px">
+                        <List.Item>Add to Favorites...</List.Item>
+                        <List.Item>Organize Favorites...</List.Item>
+                        <List.Divider />
+                        <List.Item>No favorites added yet</List.Item>
+                    </List>
+                }, {
+                    name: 'Tools',
+                    list: <List width="200px">
+                        <List.Item>
+                            <List width="150px">
+                                <List.Item>Read Mail</List.Item>
+                                <List.Item>New Message...</List.Item>
+                                <List.Item>Send a Link...</List.Item>
+                                <List.Item>Send Page</List.Item>
+                                <List.Divider />
+                                <List.Item>Read News</List.Item>
+                            </List>
+                            Mail and News
+                        </List.Item>
+                        <List.Item>Synchronize</List.Item>
+                        <List.Item>Windows Update</List.Item>
+                        <List.Divider />
+                        <List.Item>Show related links</List.Item>
+                        <List.Divider />
+                        <List.Item>Internet Options</List.Item>
+                    </List>
+                }, {
+                    name: 'Help',
+                    list: <List width="200px">
+                        <List.Item>Contents and Index</List.Item>
+                        <List.Item>Tip of the Day</List.Item>
+                        <List.Item>For Netscape Users</List.Item>
+                        <List.Item>Tour</List.Item>
+                        <List.Item>Online Support</List.Item>
+                        <List.Item>Send Feedback</List.Item>
+                        <List.Divider />
+                        <List.Item>About Internet Explorer</List.Item>
+                    </List>
+                }]}
             >
-                <Frame bg="white" boxShadow="$in" h="100%" w="100%" padding="0px 5px">
-                    <p>
-                        Blog
-                    </p>
-                </Frame>
+                <BlogWindow />
             </Modal>}
 
             {showThoughtsWindow && <Modal
                 width={isMobile ? '95vw' : '90vw'}
+                maxWidth="1280px"
                 height={isMobile ? '85vh' : '80vh'}
+                maxHeight="720px"
                 title="Thoughts"
                 zIndex={1}
                 defaultPosition={isMobile ? { x: 10, y: 20 } : { x: 45, y: 10 }}
